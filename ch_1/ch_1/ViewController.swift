5f5c3bc509e7080ffb8364b486cbb86fca3e0fc8
formegusto <formegusto@gmail.com>
2022-01-03 00:46:02 +0900

    아 챕터 1 끄으으으트~~

--- ch_1/ch_1/ViewController.swift
+++ ch_1/ch_1/ViewController.swift
@@ -11,7 +11,27 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let myButton = UIButton.init(type: .system)
+        myButton.setTitle("MyButton", for: .normal)
+        
+        self.view.addSubview(myButton)
+        
+        myButton.translatesAutoresizingMaskIntoConstraints = false
+        myButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+//        myButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+        myButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: -100).isActive = true
+        
+        
+        // Label
+        let myLabel = UILabel.init()
+        myLabel.text = "Hello, Swift :)"
+        
+        self.view.addSubview(myLabel)
+        
+        myLabel.translatesAutoresizingMaskIntoConstraints = false
+        myLabel.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 30).isActive = true
+        myLabel.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 30).isActive = true
     }
 
 
