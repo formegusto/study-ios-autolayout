33aa34e96750e067190edbbc3be7cdd9a700fbe1
formegusto <formegusto@gmail.com>
2022-01-03 22:59:24 +0900

    calc layout

---
+++ ch_2_calculator/ch_2_calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ch_2_calculator
+//
+//  Created by formegusto on 2022/01/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
