46e4e76d970bc3c7d68ae1f97ce81c32035e81a7
formegusto <formegusto@gmail.com>
2022-01-25 23:19:40 +0900

    size 마무으리!

---
+++ ch_13_size/ch_13_size/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ch_13_size
+//
+//  Created by formegusto on 2022/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
